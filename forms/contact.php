afa9b9aab5d58e32b1a05a55f1dc8f744c07e3e3
Fermanuel <manuelhola9@gmail.com>
2024-01-10 22:25:17 -0800

    modificaciones en el formulario de contacto

--- forms/contact.php
+++ forms/contact.php
@@ -1,19 +1,34 @@
 <?php
-  $to = 'manuelhola9@gmail.com';
-  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-  $from = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-  $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
-  $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+
+// Definir destinatario 
+$to = 'manuelhola9@gmail.com';
+
+// Obtener datos del formulario
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+$from = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+
+// Validar email
+if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
+
+  // Armar cabeceras
+  $headers = [
+    'From' => ($name?"<$name> ":'') . $from,
+    'X-Mailer' => 'PHP/' . phpversion()
+  ];
   
-  if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
-      $headers = ['From' => ($name?"<$name> ":'').$from,
-              'X-Mailer' => 'PHP/' . phpversion()
-             ];
-  
-      mail($to, $subject, $message."\r\n\r\nfrom: ".$_SERVER['REMOTE_ADDR'], $headers);
-      die('OK');
-      
-  } else {
-      die('Invalid address');
-  }
+  // Enviar email 
+  mail($to, $subject, $message."\r\n\r\nfrom: " . $_SERVER['REMOTE_ADDR'], $headers);
+
+  // Mostrar mensaje de éxito
+  die('OK');
+
+} else {
+
+  // Mostrar error de email inválido
+  die('Invalid address'); 
+
+}
+
 ?>
