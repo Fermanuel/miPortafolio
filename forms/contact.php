871fea9588ba65615bc5cfead3c09d9900779ace
Fermanuel <manuelhola9@gmail.com>
2024-01-13 19:55:34 -0800

    Funcionalidad del formulario

--- forms/contact.php
+++ forms/contact.php
@@ -1,34 +1,12 @@
 <?php
 
-// Definir destinatario 
-$to = 'manuelhola9@gmail.com';
+$resend = Resend::client('re_PxE8v9RD_ChuFRpX81jyxH9rvfh1x1PmH');
 
-// Obtener datos del formulario
-$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-$from = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-$subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
-$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
-
-// Validar email
-if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
-
-  // Armar cabeceras
-  $headers = [
-    'From' => ($name?"<$name> ":'') . $from,
-    'X-Mailer' => 'PHP/' . phpversion()
-  ];
-  
-  // Enviar email 
-  mail($to, $subject, $message."\r\n\r\nfrom: " . $_SERVER['REMOTE_ADDR'], $headers);
-
-  // Mostrar mensaje de éxito
-  die('OK');
-
-} else {
-
-  // Mostrar error de email inválido
-  die('Invalid address'); 
-
-}
-
-?>
+$resend->emails->send([
+  'from' => 'onboarding@resend.dev',
+  'to' => 'manuelhola9@gmail.com',
+  'email' => $_POST['email'],
+  'name' => $_POST['name'],
+  'subject' => $_POST['subject'],
+  'html' => '<p>' . $_POST['message'] . '</p>'
+]);
